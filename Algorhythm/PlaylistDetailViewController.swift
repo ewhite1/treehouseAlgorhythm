9ffaf3a7b6ebb3ab3bf5e7206835c34b4405d0f3
Edward White <edwardwhite1@outlook.com>
2015-12-15 22:02:41 -0600

    Renamed the Viewcontrollers, Cleaned the build, and added a second view controller. No segue setup yet.

---
+++ Algorhythm/PlaylistDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PlaylistDetailViewController.swift
+//  Algorhythm
+//
+//  Created by Edward White on 12/15/15.
+//  Copyright © 2015 PositiveCapitalism. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
