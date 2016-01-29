9f22a0b1df613d44d19b47a35f22e50d4391a7e9
Edward White <edwardwhite@outlook.com>
2016-01-29 06:33:03 -0600

    added gestures to each playlist. added icon for program, and launch screen assets. App beta 1.0 completed!

--- Algorhythm/PlaylistMasterViewController.swift
+++ Algorhythm/PlaylistMasterViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 
 class PlaylistMasterViewController: UIViewController {
 
-  
-    @IBOutlet weak var playlistImageView0: UIImageView! //I know this is bad! sorry
+    var playlistsArray: [UIImageView] = []
+    @IBOutlet weak var playlistImageView0: UIImageView! //I know this isn't named well
+    @IBOutlet weak var playlistImageView1: UIImageView!
+    @IBOutlet weak var playlistImageView2: UIImageView!
+    @IBOutlet weak var playlistImageView3: UIImageView!
+    @IBOutlet weak var playlistImageView4: UIImageView!
+    @IBOutlet weak var playlistImageView5: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        playlistsArray += [playlistImageView0, playlistImageView1, playlistImageView2, playlistImageView3, playlistImageView4, playlistImageView5]
+        
+        for index in 0..<playlistsArray.count {
+            let playlist = Playlist(index: index)
+            let playlistImageView = playlistsArray[index]
+            
+            playlistImageView.image = playlist.icon
+            playlistImageView.backgroundColor = playlist.backgroundColor
+        }
+        
         
         let playlist = Playlist(index: 0)
         playlistImageView0.image = playlist.icon
@@ -26,14 +43,15 @@ class PlaylistMasterViewController: UIViewController {
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "showPlaylistDetailSegue" {
+        
+        let playlistImageView = sender!.view as! UIImageView
+        if let index = playlistsArray.indexOf(playlistImageView) {
             let playlistDetailController = segue.destinationViewController as! PlaylistDetailViewController
-            
-            playlistDetailController.playlist = Playlist(index: 0)
-            
+            playlistDetailController.playlist = Playlist(index: index)
         }
-    }
     
+}
+
     @IBAction func showPlaylistDetail(sender: AnyObject) {
         performSegueWithIdentifier("showPlaylistDetailSegue", sender: sender)
     }
